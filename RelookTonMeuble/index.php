7dd4665575ee49a42be4f857c0b5707d2829fda3
QuentinSarra <qsarrazin35@gmail.com>
2018-12-03 11:13:23 +0100

    Connexion + Menu html

---
+++ RelookTonMeuble/index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html><html class="menu">
+<html>
+
+<head>
+
+    <meta charset="utf-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+    <meta name="google" value="notranslate"/>
+    <title>Side Menu</title>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>RelookTonMeuble</title>
+    <meta name="keywords" content="Mettre, des, mots, ici">
+    <meta name="description" content="Courte description ici.">
+	<meta name="author" content="Trébern Jérôme, Sarazin Quentin, Lucas Douillard">
+	<meta name="copyright" content="© RelookTonMeuble 2018" />
+    
+    <link rel="stylesheet" media="screen" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    
+</head>
+<body>
+
+
+
+    <?php include "menu.html";?>
+    
+    
+
+    <section>
+        <div style="background-color: antiquewhite; margin-left: 55px;" class="row">
+            <div class="col-md-6">A</div>
+            <div class="col-md-6">B</div>
+        
+        
+        </div>
+        
+    
+    
+    
+    
+    
+    </section>
+    
+    
+  
+
+			
+  
+  
+</body>
+</html>
