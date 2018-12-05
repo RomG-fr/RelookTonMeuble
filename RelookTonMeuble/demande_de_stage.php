80f42ac7c7a2da9b43b2c805380973e38e498f6b
QuentinSarra <qsarrazin35@gmail.com>
2018-12-05 09:39:21 +0100

    reprise de la dernière fois

--- RelookTonMeuble/demande_de_stage.php
+++ RelookTonMeuble/demande_de_stage.php
@@ -1,7 +1,9 @@
 <html>
 	<head>
 		<title>Demande de stage</title>
-		<meta charset="utf-8">
+	  <link rel="stylesheet" type="text/css" href="css/style.css">
+	  <link rel="stylesheet" type="text/css" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+	  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 	</head>
 	<body>
 		<div>
