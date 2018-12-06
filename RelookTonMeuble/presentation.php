95d27db6fcca82a42ffeb8ca8e21a7887733a627
QuentinSarra <qsarrazin35@gmail.com>
2018-12-06 19:13:33 +0100

    Ajout des pages

--- RelookTonMeuble/presentation.php
+++ RelookTonMeuble/presentation.php
@@ -1,94 +1,58 @@
+<!DOCTYPE html>
 <html>
 <head>
-	<title>Contact</title>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>AdminLTE 2 | Dashboard</title>
-	<!-- Tell the browser to be responsive to screen width -->
-	<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-	<!-- Bootstrap 3.3.7 -->
-	<link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
-	<!-- Font Awesome -->
-	<link rel="stylesheet" href="bower_components/font-awesome/css/font-awesome.min.css">
-	<!-- Ionicons -->
-	<link rel="stylesheet" href="bower_components/Ionicons/css/ionicons.min.css">
-	<!-- jvectormap -->
-	<link rel="stylesheet" href="bower_components/jvectormap/jquery-jvectormap.css">
-	<!-- Theme style -->
-	<link rel="stylesheet" href="dist/css/AdminLTE.min.css">
-	<!-- AdminLTE Skins. Choose a skin from the css/skins
-	    folder instead of downloading all of them to reduce the load. -->
-	<link rel="stylesheet" href="dist/css/skins/_all-skins.min.css">
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>AdminLTE 2 | Dashboard</title>
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  <!-- Bootstrap 3.3.7 -->
+  <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="bower_components/font-awesome/css/font-awesome.min.css">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="bower_components/Ionicons/css/ionicons.min.css">
+  <!-- jvectormap -->
+  <link rel="stylesheet" href="bower_components/jvectormap/jquery-jvectormap.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="dist/css/AdminLTE.min.css">
+  <!-- AdminLTE Skins. Choose a skin from the css/skins
+       folder instead of downloading all of them to reduce the load. -->
+  <link rel="stylesheet" href="dist/css/skins/_all-skins.min.css">
 
-	<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-	<!--[if lt IE 9]>
-	<script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-	<![endif]-->
+  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
 
-	<!-- Google Font -->
-	<link rel="stylesheet"
+  <!-- Google Font -->
+  <link rel="stylesheet"
         href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
 </head>
 <body class="hold-transition skin-blue sidebar-mini">
-	<div class="wrapper">
-	 	<?php include "menu.html";?>
+<div class="wrapper">
 
-	 <!-- Content Wrapper. Contains page content -->
-  	<div class="content-wrapper">
+    <?php include "menu.html"; ?>
+    
+    
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
     <!-- Content Header (Page header) -->
     <section class="content-header">
-      
+      <h1>
+        Présentation
+      </h1>
+      <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+        <li class="active">Dashboard</li>
+      </ol>
+    </section>
+
     <!-- Main content -->
     <section class="content">
-
-
-	 	 <div class="row" style="margin-left:55px;">
-			<div class="col-md-8">
-				<h2>Contactez-nous</h2>
-				<p>Magasin</p>
-				<p>adresse</p>
-				<p>ville</p>
-				<p>horaire</p>
-				<p>adresse mail</p>
-			</div>
-			<div class="col-md-4">
-				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2663.7452668047736!2d-1.6405629847062004!3d48.11514947922164!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x480edee4490f4333%3A0x3c4238e97734ee99!2sISTIC!5e0!3m2!1sfr!2sfr!4v1543834583937" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
-			</div>
-		</div>
-		<div class="row" style="margin-left:55px;">
-			<div class="col-md-8">
-				<h2>Pour un relooking de meuble, envoyez nous un message avec une photo de celui-ci</h2>
-				<form>
-					<table>
-						<tr>
-							<td><p>Nom*</p></td>
-							<td><input type="text" name="nom" size="48"/></td>
-						</tr>
-						<tr>
-							<td><p>Email*</p></td>
-							<td><input type="text" name="mail" size="48"/></td>
-						</tr>
-						<tr>
-							<td><p>Titre*</p></td>
-							<td><input type="text" name="titre" size="48"/></td>
-						</tr>
-						<tr>
-							<td><p>Message*</p></td>
-							<td><textarea name="corps_de_texte" rows="8" cols="32"></textarea></td>
-						</tr>
-						<tr>
-							<td><p>Photo du meuble*</p></td>
-							<td><input name="MAX_FILE_SIZE" type="hidden" value="3000" /><input name="fichier" type="file" /></td>
-						</tr>
-					</table>
-					<input type="submit" name="envoyer" value="Envoyer"/>
-				</form>
-			</div>	
-		</div>
-	 </section>	
-
+     test
     </section>
     <!-- /.content -->
   </div>
@@ -317,10 +281,5 @@
 <script src="dist/js/pages/dashboard2.js"></script>
 <!-- AdminLTE for demo purposes -->
 <script src="dist/js/demo.js"></script>
-<script type="text/javascript" src="https://cdn.jsdelivr.net/jquery/latest/jquery.min.js"></script>
-<script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
-<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
-<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />
-	
 </body>
 </html>
