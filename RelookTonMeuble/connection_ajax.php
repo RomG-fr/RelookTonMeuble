83a970a37bb1fbe8dc7142b883bb1d77feb44af5
Jérôme <jerome.trebern@gmail.com>
2019-01-07 13:02:22 +0100

    Design connexion : Fini

--- RelookTonMeuble/connection_ajax.php
+++ RelookTonMeuble/connection_ajax.php
@@ -46,20 +46,16 @@
 		  <!-- Google Font -->
 		  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
           <link rel="stylesheet" type="text/css" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
-          <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-          <link rel="stylesheet" type="text/css" href="css/style.css">
-			<link rel="stylesheet" href="css/perso_custom.css">
+          <link rel="stylesheet" type="text/css" href="css/style_connexion.css">
+          <link rel="stylesheet" href="css/perso_custom.css">
 
 </head>
 
 <body>
-    <div id="resultat">
-        <!--affiche un retour en jQuery au visiteur -->
-    </div>
+    
 
      <body class="hold-transition sidebar-mini">
 <div class="wrapper">
-
     <?php include "menu.html"; ?>
 
 
@@ -77,46 +73,40 @@
 
     <!-- Main content -->
     <section class="content">
-    	<form onsubmit='return verif_vide()'>
-	        <p>
-	        Nom d'utilisateur : <input type="text" placeholder="exemple@mail.com" id="login" />
-	        Mot de passe : <input type="password" placeholder="********" id="mdp" />
-	        <input type="submit" id="submit" value="Se connecter !" />
-	        </p>
-	    </form>
+        
+        <div id="resultat">
+        <!--affiche un retour en jQuery au visiteur -->
+        </div>
 	    <div class="cont_principal">
 		    <div class="cont_centrar">
-		    <div class="cont_login">
-		      <form action="">
-		      <div class="cont_tabs_login">
-		        <ul class='ul_tabs'>
-		          <li class="active"><a href="#" onclick="sign_in()">Se Connecter</a>
-		          <span class="linea_bajo_nom"></span>
-		          </li>
+                <div class="cont_login">
+                      <form onsubmit='return verif_vide()'>
+                          <div class="cont_tabs_login">
+                            <ul class='ul_tabs'>
+                              <li class="active">
+                                <a href="#" onclick="sign_in()">Se Connecter</a>
+                                <span class="linea_bajo_nom"></span>
+                              </li>
 
-		        </ul>
-		        </div>
-		    <div class="cont_text_inputs">
-		        <input type="text" class="input_form_sign " placeholder="NAME" name="name_us"  />
+                            </ul>
+                          </div>
+                    <div class="cont_text_inputs">
 
-		      <input type="text" class="input_form_sign d_block active_inp" placeholder="EMAIL" name="emauil_us" id="login"/>
+                          <input type="text" class="input_form_sign d_block active_inp" placeholder="Email" name="emauil_us" id="login"/>
 
-		      <input type="password" class="input_form_sign d_block  active_inp" placeholder="PASSWORD" name="pass_us" id="mdp" />
-		     <input type="password" class="input_form_sign" placeholder="CONFIRM PASSWORD" name="conf_pass_us" />
+                          <input type="password" class="input_form_sign d_block  active_inp" placeholder="Mot de passe" name="pass_us" id="mdp" />
+                         
 
-		      <a href="#" class="link_forgot_pass d_block" >Mot de passe oublié ?</a>
-		  <div class="terms_and_cons d_none">
-		      <p><input type="checkbox" name="terms_and_cons" /> <label for="terms_and_cons">Accept  Terms and Conditions.</label></p>
+                          <a href="#" class="link_forgot_pass d_block" >Mot de passe oublié ?</a>
+                         
+                        </div>
+                  <div class="cont_btn">
+                       <button class="btn_sign">S'inscrire</button>
 
-		      </div>
-		        </div>
-		  <div class="cont_btn">
-		       <button class="btn_sign">SIGN IN</button>
+                    </div>
 
-		        </div>
-
-		      </form>
-		      </div>
+                      </form>
+                  </div>
 
 		    </div>
 		  </div>
