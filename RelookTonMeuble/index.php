0d8026c3cf216d04888ca863b8f8d921851a3cd8
QuentinSarra <qsarrazin35@gmail.com>
2019-01-07 21:18:28 +0100

    infobulle

--- RelookTonMeuble/index.php
+++ RelookTonMeuble/index.php
@@ -32,6 +32,90 @@
   <!-- Google Font -->
   <link rel="stylesheet"
         href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+  <script type="text/javascript" src="http://code.jquery.com/jquery-latest.js"></script>
+    <script type="text/javascript">
+    //permet d'afficher l'infobulle
+    $(document).ready(function() {
+     
+      // Sélectionner tous les liens ayant l'attribut rel valant tooltip
+      $('a[rel=tooltip]').mouseover(function(e) {
+     
+        // Récupérer la valeur de l'attribut title et l'assigner à une variable
+        var tip = $(this).attr('title');   
+     
+        // Supprimer la valeur de l'attribut title pour éviter l'infobulle native
+        $(this).attr('title','');
+     
+        // Insérer notre infobulle avec son texte dans la page
+        $(this).append('<div id="tooltip"><div class="tipHeader"></div><div class="tipBody">' + tip + '</div><div class="tipFooter"></div></div>');    
+     
+        // Ajuster les coordonnées de l'infobulle
+        $('#tooltip').css('top', e.pageY + 10 );
+        $('#tooltip').css('left', e.pageX + 20 );
+     
+        // Faire apparaitre l'infobulle avec un effet fadeIn
+        $('#tooltip').fadeIn('500');
+        $('#tooltip').fadeTo('10',0.8);
+     
+      }).mousemove(function(e) {
+     
+        // Ajuster la position de l'infobulle au déplacement de la souris
+        $('#tooltip').css('top', e.pageY + 10 );
+        $('#tooltip').css('left', e.pageX + 20 );
+     
+      }).mouseout(function() {
+     
+        // Réaffecter la valeur de l'attribut title
+        $(this).attr('title',$('.tipBody').html());
+     
+        // Supprimer notre infobulle
+        $(this).children('div#tooltip').remove();
+     
+      });
+     
+    });
+    </script>
+    <style>
+      /* {
+        color:#aaa;text-decoration:none;
+        cursor:pointer;
+        cursor:hand}
+      a:hover {
+        color:#000;
+        text-decoration:none;}
+       */
+       
+      /* Infobulle */
+      
+      /* style infobulle*/
+       
+      #tooltip {
+        position:absolute;
+        z-index:9999;
+        color:#000;
+        font-size:20px;
+        width:180px;
+       
+      }
+       
+      #tooltip .tipHeader {
+        height:8px;
+        background:url(images/tipHeader.gif) no-repeat;
+      }
+       
+      /* hack IE */
+      *html #tooltip .tipHeader {margin-bottom:-6px;}
+       
+      #tooltip .tipBody {
+        background-color:#fff;
+        padding:20px;
+      }
+       
+      #tooltip .tipFooter {
+        height:8px;
+        background:url(images/tipFooter.gif) no-repeat;
+      }
+    </style>
 </head>
 <body class="hold-transition sidebar-mini">
 <div class="wrapper">
@@ -59,10 +143,14 @@
         <p>Armoire, commode, table, chaise... Peu de meubles résistent au pouvoir rajeunissant d'une peinture fraîche et éclatante. Et c'est tant mieux ! La peinture, en pot ou en bombe, reste la solution la plus accessible pour le petit relooking pas cher d'un meuble. Une cuisine en pin se modernisera sous l'effet d'une couleur actuelle comme le noir, le gris ou des teintes plus douces et poudrées. Vous pouvez par ailleurs préférer une patine ou une céruse à la peinture pour éclaircir et offrir un style différent à un vieux meuble en bois. Enfin, il n'est pas toujours nécessaire de repeindre le meuble dans sa totalité pour produire un effet waouh. Ne colorer que les piètements, qu'une moitié de porte ou de dossier suffit souvent à lui donner du cachet. Voici tout plein d'idées pour vous inspirer et relooker un meuble rustique ou chiné avec de la peinture.</p>
       <br  />
       <br  />
-      <video width="320" height="240"  controls>
-        <source src='video/relooking_meuble.mp4' type="video/mp4">
-        Votre navigateur ne supporte pas la balise vidéo.
-      </video>
+      <div id="paragraph">
+      <a rel="tooltip" title="exemple de realisation...");>
+        <video width="320" height="240" controls>
+          <source src='video/relooking_meuble.mp4' type="video/mp4">
+          Votre navigateur ne supporte pas la balise vidéo.
+        </video>
+      </a>
+    </div>
     </center>
     </section>
     <!-- /.content -->
