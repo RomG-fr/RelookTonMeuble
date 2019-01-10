5bd34db5194ad9bb95d8a9689e37b29d0068e4fd
QuentinSarra <qsarrazin35@gmail.com>
2019-01-10 16:44:00 +0100

    detail2

--- RelookTonMeuble/detail.php
+++ RelookTonMeuble/detail.php
@@ -3,7 +3,7 @@ session_start();
 ?>
 <html>
 <head>
-	<title>Contact</title>
+	<title>Detail</title>
 	<link rel="stylesheet" type="text/css" href="css/style.css">
   <link rel="stylesheet" type="text/css" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
